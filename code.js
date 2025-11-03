109da584157a70200c0b7a38c27f73363b080c40
KarolZSE <karol.sulek@zse.krakow.pl>
2025-11-03 16:25:13 +0100

    Added pipes for a new minigame, added a ability to place color (later will change to placing a pipe) on the container by dragging it from the pipes, also added rotation for the elements in the container (they will rotate if the player touches them)

---
+++ code.js
@@ -0,0 +1,134 @@
+    const container = document.getElementById('container');
+    for (let i = 0; i < 100; i++) {
+        const square = document.createElement('div');
+        square.textContent = i;
+        square.style.background = '#664c28';
+        if (Math.random() > 0.7) square.style.background = '#80807e';
+        container.appendChild(square);
+    }
+
+    let planes = 5;
+    const contr = document.querySelectorAll('.contr');
+    contr.forEach(e => {
+        for (let i = 0; i < 10; i++) {
+            const square = document.createElement('button');
+            square.textContent = i;
+            e.appendChild(square);
+            square.onclick = function() {
+                if (!planes) {
+                    CountFilledSquares();
+                    return;
+                };
+
+                const plane = document.createElement('div');
+                plane.classList.add('plane');
+
+                const containerRect = container.getBoundingClientRect();
+
+                if (e.id == 'verticaltop') {
+                    plane.style.left = (containerRect.left + i * 60) + 'px';
+                    plane.style.top = (containerRect.top - 60) + 'px';
+                    setTimeout(() => {
+                        plane.style.top = (containerRect.top + 600) + 'px';
+                    }, 0);
+                } else if (e.id == 'verticalbottom') {
+                    plane.style.left = (containerRect.left + i * 60) + 'px';
+                    plane.style.top = (containerRect.top + 600) + 'px';
+                    setTimeout(() => {
+                        plane.style.top = (containerRect.top - 60) + 'px';
+                    }, 0);
+                } else if (e.id == 'horizontalleft'){
+                    plane.style.left = (containerRect.left - 60) + 'px';
+                    plane.style.top = (containerRect.top + i * 60) + 'px';
+                    setTimeout(() => {
+                        plane.style.left = (containerRect.left + 600) + 'px';
+                    }, 0);
+                } else {
+                    plane.style.left = (containerRect.left + 600) + 'px';
+                    plane.style.top = (containerRect.top + i * 60) + 'px';
+                    setTimeout(() => {
+                        plane.style.left = (containerRect.left - 60) + 'px';
+                    }, 0);
+                }
+                setTimeout(() => {
+                    plane.remove();
+                }, 1000);
+
+                document.body.appendChild(plane);
+
+                for (let j = 0; j < 10; j++) {
+                    let gridsquare;
+                    if (e.id == 'verticaltop') {
+                        gridsquare = container.children[i + j * 10];
+                    } else if (e.id == 'verticalbottom') {
+                        gridsquare = container.children[100 - (10 - i + j * 10)];
+                    } else if (e.id == 'horizontalleft'){
+                        gridsquare = container.children[i * 10 + j];
+                    } else {
+                        gridsquare = container.children[i * 10 + 9 - j];
+                    }
+
+                    setTimeout(() => {
+                        if (gridsquare.style.background !== 'rgb(128, 128, 126)') {
+                            gridsquare.style.background = '#000000';
+                        } 
+                    }, 100 * j);
+                }
+                square.onclick = '';
+                planes--;
+            }
+        }
+    });
+
+    function CountFilledSquares() {
+        let count = stonescount = 0;
+        const child = container.children;
+
+        for (let i = 0; i < 100; i++) {
+            const childbg = child[i].style.background;
+            if (childbg == 'rgb(0, 0, 0)') {
+                count++
+            }
+
+            if (childbg == 'rgb(128, 128, 126)') {
+                stonescount++
+            }
+        }
+
+        console.log(`You had filled in ${count}, which equals to ${(count / (100 - stonescount) * 100).toFixed(2)}% of the board (excluding bolders).`);
+    }
+
+
+    planes = 0;
+    container.innerHTML = '';
+    for (let i = 0; i < 100; i++) {
+        const square = document.createElement('div');
+        square.textContent = i;
+        // square.style.background = '';
+        // if (Math.random() > 0.7) square.style.background = '#80807e';
+        container.appendChild(square);
+
+        let deg = 1
+        square.onclick = function() {
+            square.style.transform = `rotate(${deg * 90}deg)`;
+            deg++;
+        }
+
+        square.addEventListener('dragover', (e) => {
+            e.preventDefault();
+        })
+
+        square.addEventListener('drop', (e) => {
+            e.preventDefault();
+            const color = e.dataTransfer.getData('text');
+            square.style.background = color;
+        });
+    }
+
+    const pipes = document.querySelectorAll('#pipes div');
+
+    pipes.forEach(pipe => {
+        pipe.addEventListener('dragstart', (e) => {
+            e.dataTransfer.setData('text', pipe.style.background);
+        });
+    });
